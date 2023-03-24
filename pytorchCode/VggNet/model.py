eb1a5501956e6c7024ccf1d41609a323aadc4c49
office <clg19980805@outlook.com>
2023-03-24 18:04:20 +0800

    update

---
+++ pytorchCode/VggNet/model.py
@@ -0,0 +1,108 @@
+import torch
+import torch.nn as nn
+
+
+class VGG(nn.Module):
+    def __init__(self, features, num_classes=1000, init_weights=False):
+        super(VGG, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Linear(512*7*7, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, num_classes),
+            nn.ReLU(inplace=True)
+        )
+        if init_weights:
+            self._initialize_weihgts()
+
+    def _initialize_weihgts(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                # nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                nn.init.xavier_uniform_(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias)
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                nn.init.constant_(m.bias)
+
+def make_features(self, cfg):
+    in_channels = 3
+    layers = []
+    for v in cfg:
+        if v == "M":
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            layers += [nn.Conv2d(in_channels, v, kernel_size=3, padding=1), nn.ReLU(inplace=True)]
+
+    return nn.Sequential(*layers)
+
+cfgs = {
+    'vgg11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'vgg19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+def vgg(model_name='vgg16', **kwargs):
+    assert model_name in cfgs, "Warring {} is not in cfgs!".format(model_name)
+    cfg = cfgs[model_name]
+    model = VGG(make_features(cfg), **kwargs)
+    return model
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ pytorchCode/VggNet/train.py
@@ -0,0 +1,27 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
